f4aeda7a360311e1d4d993c97c0f3b1cfdb74265
Afanasev Aleksandr <104716639+afonyAAAA@users.noreply.github.com>
2023-02-01 15:16:20 +0700

    Add files via upload

---
+++ Country.kt
@@ -0,0 +1,10 @@
+package ru.fi.mycursprojectgotravel.model
+
+import java.io.File
+
+data class Country(
+    var id: String = "",
+    var nameCountry: String = "",
+    var generalDescription: String = "",
+    var imageCountry: String = ""
+)
